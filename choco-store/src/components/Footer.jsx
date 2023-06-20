0687a383071191c33b108078e0869ebe16ff0a7f
Marilyn Guillaume <contact@atdr-marketing.fr>
2023-06-20 10:43:23 +0200

    changement 20 juin V3

--- choco-store/src/components/Footer.jsx
+++ choco-store/src/components/Footer.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="column">
-                <h1 className="title">Title 1</h1>
-                <p className="text">Text 1</p>
-            </div>
-            <div className="column">
-                <h1 className="title">Title 2</h1>
-                <p className="text">Text 2</p>
-            </div>
-            <div className="column">
-                <div className="social-media">
-                    <i className="fab fa-facebook-f"></i>
-                    <i className="fab fa-instagram"></i>
-                    <i className="fab fa-twitter"></i>
-                </div>
-            </div>
-        </footer>
-    );
-}
+  return (
+    <footer className="footer">
+      <div className="column">
+        <p className="title">Choco Pap</p>
+        <p className="text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </p>
+      </div>
+      <div className="column">
+        <p className="title">Contact</p>
+        <p className="text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </p>
+      </div>
+      <div className="column">
+        <div className="social-media">
+          <i className="fab fa-facebook-f"></i>
+          <i className="fab fa-instagram"></i>
+          <i className="fab fa-twitter"></i>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
