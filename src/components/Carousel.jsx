6c3ddbd12b8b430452a1d6237f25e2cfb432c40d
Marilyn Guillaume <contact@atdr-marketing.fr>
2023-07-17 14:12:12 +0200

    Co-authored-by: clementnv <clementnv@users.noreply.github.com>

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,31 +1,53 @@
 import React from "react";
-import { Carousel as CarouselResponsive } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import Slider from "react-slick";
+
 import "../styles/Carousel.css";
-import accueil1 from "./accueil1.jpg";
-import accueil2 from "./accueil1.jpg";
-import accueil3 from "./accueil1.jpg";
+
+import accueil1 from "../images/accueil1.jpg";
+import accueil2 from "../images/accueil2.jpg";
+import accueil3 from "../images/accueil3.jpg";
 
 const Carousel = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <CarouselResponsive>
-      <div>
+    <Slider {...settings}>
+      {/* Slide 1 */}
+      <div className="carousel">
         <img src={accueil1} alt="Slide 1" />
-        <p className="legend">Legend 1</p>
+        <div className="contentSlider">
+          <h1 className="carousel-title">Choco Pap</h1>
+          <a href="/boutique" className="boutique-button">
+            Voir la boutique
+          </a>
+        </div>
       </div>
-      <div>
+      {/* Slide 2 */}
+      <div className="carousel">
         <img src={accueil2} alt="Slide 2" />
-        <p className="legend">Legend 2</p>
+        <div className="contentSlider">
+          <h1 className="carousel-title">Choco Pap</h1>
+          <a href="/boutique" className="boutique-button">
+            Voir la boutique
+          </a>
+        </div>
       </div>
-      <div>
+      {/* Slide 3 */}
+      <div className="carousel">
         <img src={accueil3} alt="Slide 3" />
-        <p className="legend">Legend 3</p>
+        <div className="contentSlider">
+          <h1 className="carousel-title">Choco Pap</h1>
+          <a href="/boutique" className="boutique-button">
+            Voir la boutique
+          </a>
+        </div>
       </div>
-      {/* <h1 className="carousel-title">Choco Pap</h1>
-        <a href="/boutique" className="boutique-button">
-          Voir la boutique
-        </a> */}
-    </CarouselResponsive>
+    </Slider>
   );
 };
 export default Carousel;
