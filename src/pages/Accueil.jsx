ca29c6ecddc97b30567aec970391a418b0422421
Marilyn Guillaume <contact@atdr-marketing.fr>
2023-08-01 21:01:20 +0200

    bc filter

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "../components/Carousel";
 import PRODUITS from "../data/products.json";
+import { Link } from "react-router-dom";
 
 const Accueil = () => {
   return (
